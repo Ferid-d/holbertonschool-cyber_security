48c06a3c8974fee458a92330caefd911faac1bcd
= <=>
2026-02-02 15:39:54 +0400

    wefdcxx

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt "$1" && john --show "$1" | awk -F: 'NR>0 && $2!="" {print $2}' > 4-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt "$1" --format=Raw-MD5 && john --show "$1" --format=Raw-MD5 | awk -F: 'NF>1 {print $2}' > 4-password.txt
