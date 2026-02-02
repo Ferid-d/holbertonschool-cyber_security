908ac6fc2dfdfd11e025cdb8ce9e9aa816ce984c
= <=>
2026-02-02 15:29:07 +0400

    wefdcxx

--- cybersecurity_basics/0x03_cryptography_basics/3-password_hash.sh
+++ cybersecurity_basics/0x03_cryptography_basics/3-password_hash.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-SALT=$(openssl rand -base64 12 | cut -c1-16)
-echo -n "${SALT}$1" | openssl dgst -sha512 > 3_hash.txt
+echo -n "$(openssl rand -base64 12 | cut -c1-16)$1" | openssl dgst -sha512 > 3_hash.txt
