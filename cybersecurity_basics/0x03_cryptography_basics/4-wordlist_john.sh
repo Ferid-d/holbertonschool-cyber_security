c58aaece34ddfb4dc36dc4dc39218a0c65a28a39
= <=>
2026-02-02 15:38:40 +0400

    wefdcxx

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt "$1" && john --show "$1" > 4-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt "$1" && john --show "$1" | awk -F: 'NR>0 && $2!="" {print $2}' > 4-password.txt
+
