04ef03de4a1e1479924dfc05f6ed29e65c43fd1e
= <=>
2026-02-10 12:52:07 +0400

    efc

--- linux_security/0x03_protocols_servers/6-smtp.sh
+++ linux_security/0x03_protocols_servers/6-smtp.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-CONFIG=$(postconf -h smtpd_tls_security_level 2>/dev/null)
-if [ -z "$CONFIG" ] || [ "$CONFIG" == "none" ]; then
-    echo "STARTTLS not configured"
-else
-    echo "smtpd_tls_security_level = $CONFIG"
-fi
+postconf smtpd_tls_security_level || echo "STARTTLS not configured"
